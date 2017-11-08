0a449934cd0c58dec1a0b2fff05b0c5df6fcb906
Arash <arash.e.alikhani@gmail.com>
2017-11-09 01:56:01 +0330

    add iransans font, setup js and css scaffold and config laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,25 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/public.scss', 'public/css')
+    .sass('resources/assets/sass/student.scss', 'public/css')
+    .sass('resources/assets/sass/admin.scss', 'public/css');
+
+mix.js('resources/assets/js/public.js', 'public/js')
+    .js('resources/assets/js/student.js', 'public/js')
+    .js('resources/assets/js/admin.js', 'public/js');
+
+mix.scripts([
+    'node_modules/jquery/dist/jquery.min.js',
+    'resources/assets/semantic/semantic.min.js',
+    'node_modules/axios/dist/axios.min.js',
+    'node_modules/lodash/lodash.min.js'
+], 'public/js/vendor.js');
+
+mix.styles([
+    'resources/assets/semantic/semantic.rtl.min.css',
+    'resources/assets/css/IRANSans.css'
+], 'public/css/vendor.css');
+
+mix.copyDirectory('resources/assets/semantic/themes/default', 'public/css/themes/default');
+mix.copyDirectory('resources/assets/fonts', 'public/fonts');
